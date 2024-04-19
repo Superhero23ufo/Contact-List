06ffe1d72aa95481746dc332b14b78e7fd3fcb78
Your Name <you@example.com>
2024-04-18 18:58:39 -0700

    redy

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Authenticate from './components/Authenticate'
+import SignUpForm from './components/SignUpForm'
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  return (
+    <>
+      <SignUpForm setToken={setToken} />
+      <Authenticate token={token} />
+    </>
+  )
+}
+
+export default App
+
+
